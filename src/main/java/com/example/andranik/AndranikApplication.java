652f8c1274c108e5f679b5eef4628e45b505e644
unknown <Student@305-5.spobip-bel.ru>
2024-02-03 14:17:05 +0300

    Title

---
+++ src/main/java/com/example/andranik/AndranikApplication.java
@@ -0,0 +1,13 @@
+package com.example.andranik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndranikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndranikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/andranik/AndranikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.andranik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndranikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
